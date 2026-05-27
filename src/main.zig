ec7c3bd22381a13a7a65d93c8ebfa6b9431567d4
Yamin Shihab <yaminshihab123@gmail.com>
2026-05-26 19:15:25 -0600

    feat!: optimize structures and memory layouts

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,24 @@ const Random = std.Random;
 const math = std.math;
 const mem = std.mem;
 
-const Population = struct {
-    const ELITE_COUNT: usize = 50;
+const POPULATION_COUNT = 1000;
+const TOPOLOGY = [_]usize{ 2, 3, 2, 1 };
+const TOPOLOGIES = [_][]const usize{&TOPOLOGY} ** POPULATION_COUNT;
+const ELITE_COUNT: usize = 100;
 
+const MUTATION_RATE: f64 = 0.5;
+const MUTATION_STRENGTH: f64 = 0.5;
+const MUTATION_RANGE: f64 = 3.0;
+
+const TRIALS: usize = 1000;
+const XOR_INPUTS_SET = [_][]const f64{
+    &[_]f64{ 0.0, 0.0 },
+    &[_]f64{ 1.0, 0.0 },
+    &[_]f64{ 0.0, 1.0 },
+    &[_]f64{ 1.0, 1.0 },
+};
+
+const Population = struct {
     allocator: Allocator,
     networks: []Network,
 
@@ -25,71 +40,115 @@ const Population = struct {
         self.allocator.free(self.networks);
     }
 
-    fn mutate(self: *@This(), random: Random) void {
-        for (self.networks[ELITE_COUNT..]) |*network|
-            network.mutate(random);
+    fn mutate(
+        self: *@This(),
+        random: Random,
+        elite: usize,
+        rate: f64,
+        strength: f64,
+        range: f64,
+    ) void {
+        for (self.networks[elite..]) |*network|
+            network.mutate(random, rate, strength, range);
     }
 
-    fn evaluate(self: *@This(), inputs_set: []const []const f64) void {
+    fn evaluate(
+        self: *@This(),
+        inputs_set: []const []const f64,
+        fitnessFn: fn ([]const f64, []const f64) f64,
+    ) void {
         for (self.networks) |*network| {
-            network.fitness = @as(f64, @floatFromInt(inputs_set.len));
-            for (inputs_set) |inputs| {
-                const correct = @as(u1, @trunc(inputs[0])) ^ @as(u1, @trunc(inputs[1]));
-                const prediction = network.infer(inputs)[0];
-                const err = @abs(correct - prediction);
-                network.fitness -= err;
-            }
+            network.fitness = 0.0;
+            for (inputs_set) |inputs|
+                network.fitness += fitnessFn(inputs, network.infer(inputs));
         }
     }
 
-    fn select(self: *@This()) !void {
-        mem.sortUnstable(Network, self.networks, {}, comptime rankNetworks);
-        for (self.networks[ELITE_COUNT..], ELITE_COUNT..) |*network, i| {
+    fn select(self: *@This(), elite: usize) !void {
+        mem.sortUnstable(Network, self.networks, {}, struct {
+            fn lessThanFn(_: void, a: Network, b: Network) bool {
+                return a.fitness > b.fitness;
+            }
+        }.lessThanFn);
+
+        for (self.networks[elite..], elite..) |*network, i| {
             network.deinit();
-            network.* = try self.networks[i % ELITE_COUNT].clone(self.allocator);
+            network.* = try self.networks[i % elite].clone(self.allocator);
         }
     }
 };
 
-fn rankNetworks(_: void, a: Network, b: Network) bool {
-    return a.fitness > b.fitness;
-}
-
 const Network = struct {
     allocator: Allocator,
+    buffer: []f64,
     layers: []Layer,
     fitness: f64,
 
     fn init(allocator: Allocator, topology: []const usize) !@This() {
-        var layers = try allocator.alloc(Layer, topology.len - 1);
-        for (0..layers.len) |i|
-            layers[i] = try Layer.init(allocator, topology[i], topology[i + 1]);
-        return @This(){ .allocator = allocator, .layers = layers, .fitness = 0.0 };
+        var size: usize = 0;
+        for (0..topology.len - 1) |i|
+            size += topology[i + 1] * (topology[i] + 2);
+        const buffer = try allocator.alloc(f64, size);
+
+        const layers = try allocator.alloc(Layer, topology.len - 1);
+        var offset: usize = 0;
+        for (0..layers.len) |i| {
+            const input_count = topology[i];
+            const output_count = topology[i + 1];
+
+            const weights = buffer[offset .. offset + input_count * output_count];
+            offset += input_count * output_count;
+            const biases = buffer[offset .. offset + output_count];
+            offset += output_count;
+            const outputs = buffer[offset .. offset + output_count];
+            offset += output_count;
+
+            layers[i] = Layer.init(weights, biases, outputs);
+        }
+
+        return @This(){ .allocator = allocator, .buffer = buffer, .layers = layers, .fitness = 0.0 };
     }
 
     fn clone(self: *@This(), allocator: Allocator) !@This() {
         var network = @This(){
             .allocator = allocator,
+            .buffer = try allocator.alloc(f64, self.buffer.len),
             .layers = try allocator.alloc(Layer, self.layers.len),
             .fitness = self.fitness,
         };
-        for (0..network.layers.len) |i|
-            network.layers[i] = try self.layers[i].clone(allocator);
+
+        @memcpy(network.buffer, self.buffer);
+        var offset: usize = 0;
+        for (0..network.layers.len) |i| {
+            const weights = network.buffer[0..self.layers[i].weights.len];
+            offset += self.layers[i].weights.len;
+            const biases = network.buffer[offset .. offset + self.layers[i].biases.len];
+            offset += self.layers[i].biases.len;
+            const outputs = network.buffer[offset .. offset + self.layers[i].outputs.len];
+            offset += self.layers[i].outputs.len;
+
+            network.layers[i] = Layer.init(weights, biases, outputs);
+        }
+
         return network;
     }
 
     fn deinit(self: *@This()) void {
-        for (self.layers) |*layer|
-            layer.deinit();
         self.allocator.free(self.layers);
+        self.allocator.free(self.buffer);
     }
 
-    fn mutate(self: *@This(), random: Random) void {
-        for (self.layers) |*layer|
-            layer.mutate(random);
+    fn mutate(self: *@This(), random: Random, rate: f64, strength: f64, range: f64) void {
+        for (self.buffer) |*element| {
+            const chance = random.float(f64);
+            if (chance < rate) {
+                const tweak = random.floatNorm(f64) * strength;
+                element.* = math.clamp(element.* + tweak, -range, range);
+            }
+        }
     }
 
-    fn infer(self: *@This(), inputs: []const f64) []f64 {
+    fn infer(self: *@This(), inputs: []const f64) []const f64 {
         self.layers[0].forward(inputs);
         for (1..self.layers.len) |i|
             self.layers[i].forward(self.layers[i - 1].outputs);
@@ -98,59 +157,12 @@ const Network = struct {
 };
 
 const Layer = struct {
-    const MUTATION_RATE: f64 = 0.5;
-    const MUTATION_STRENGTH: f64 = 0.5;
-    const WEIGHT_RANGE: f64 = 3.0;
-    const BIAS_RANGE: f64 = 3.0;
-
-    allocator: Allocator,
     weights: []f64,
     biases: []f64,
     outputs: []f64,
 
-    fn init(allocator: Allocator, input_count: usize, output_count: usize) !@This() {
-        return @This(){
-            .allocator = allocator,
-            .weights = try allocator.alloc(f64, input_count * output_count),
-            .biases = try allocator.alloc(f64, output_count),
-            .outputs = try allocator.alloc(f64, output_count),
-        };
-    }
-
-    fn clone(self: *@This(), allocator: Allocator) !@This() {
-        const layer = @This(){
-            .allocator = allocator,
-            .weights = try allocator.alloc(f64, self.weights.len),
-            .biases = try allocator.alloc(f64, self.biases.len),
-            .outputs = try allocator.alloc(f64, self.outputs.len),
-        };
-        @memcpy(layer.weights, self.weights);
-        @memcpy(layer.biases, self.biases);
-        @memcpy(layer.outputs, self.outputs);
-        return layer;
-    }
-
-    fn deinit(self: *@This()) void {
-        self.allocator.free(self.weights);
-        self.allocator.free(self.biases);
-        self.allocator.free(self.outputs);
-    }
-
-    fn mutate(self: *@This(), random: Random) void {
-        mutateSlice(
-            random,
-            self.weights,
-            MUTATION_RATE,
-            MUTATION_STRENGTH,
-            WEIGHT_RANGE,
-        );
-        mutateSlice(
-            random,
-            self.biases,
-            MUTATION_RATE,
-            MUTATION_STRENGTH,
-            BIAS_RANGE,
-        );
+    fn init(weights: []f64, biases: []f64, outputs: []f64) @This() {
+        return @This(){ .weights = weights, .biases = biases, .outputs = outputs };
     }
 
     fn forward(self: *@This(), inputs: []const f64) void {
@@ -162,22 +174,6 @@ const Layer = struct {
         }
     }
 };
-
-fn mutateSlice(
-    random: Random,
-    slice: []f64,
-    rate: f64,
-    strength: f64,
-    range: f64,
-) void {
-    for (slice) |*element| {
-        const chance = random.float(f64);
-        if (chance < rate) {
-            const tweak = random.floatNorm(f64) * strength;
-            element.* = math.clamp(element.* + tweak, -range, range);
-        }
-    }
-}
 
 fn relu(n: f64) f64 {
     return @max(0.0, n);
@@ -192,46 +188,45 @@ fn tanh(n: f64) f64 {
 }
 
 pub fn main(init: Init) !void {
-    const TOPOLOGY = [_]usize{ 2, 3, 2, 1 };
-    const POPULATION_COUNT = 1000;
-    const TRIALS: usize = 1000;
-    const XOR_INPUTS_SET = [_][]const f64{
-        &[_]f64{ 0.0, 0.0 },
-        &[_]f64{ 1.0, 0.0 },
-        &[_]f64{ 0.0, 1.0 },
-        &[_]f64{ 1.0, 1.0 },
-    };
-
     var buffer: [4096]u8 = undefined;
     var writer = std.Io.File.stdout().writer(init.io, &buffer);
-    var stdout = &writer.interface;
-
+    const stdout = &writer.interface;
     var prng = Random.DefaultPrng.init(randomSeed(init.io));
-    var population = try Population.init(init.gpa, &[_][]const usize{&TOPOLOGY} ** POPULATION_COUNT);
+
+    var population = try Population.init(init.gpa, &TOPOLOGIES);
     defer population.deinit();
-
-    for (0..TRIALS) |i| {
-        population.mutate(prng.random());
-        population.evaluate(&XOR_INPUTS_SET);
-        try population.select();
-
-        try stdout.print(
-            "Generation #{}'s highest fitness score: {}%\n",
-            .{ i + 1, population.networks[0].fitness / 4.0 * 100.0 },
-        );
-        try writer.flush();
-    }
+    population.mutate(prng.random(), 0, 1.0, 67.0, 69_420.0);
+    try evolution(prng.random(), stdout, &population);
 
     try stdout.print("\nFittest specimen demo:\n", .{});
     for (XOR_INPUTS_SET) |inputs| {
         const outputs = population.networks[0].infer(inputs);
         try stdout.print("{} ^ {} = {}\n", .{ inputs[0], inputs[1], outputs[0] });
     }
-    try writer.flush();
+    try stdout.flush();
 }
 
 fn randomSeed(io: Io) u64 {
     var seed: u64 = undefined;
     Io.random(io, mem.asBytes(&seed));
     return seed;
+}
+
+fn evolution(random: Random, stdout: *Io.Writer, population: *Population) !void {
+    for (0..TRIALS) |i| {
+        population.mutate(random, ELITE_COUNT, MUTATION_RATE, MUTATION_STRENGTH, MUTATION_RANGE);
+        population.evaluate(&XOR_INPUTS_SET, struct {
+            fn fitnessFn(inputs: []const f64, outputs: []const f64) f64 {
+                const correct = @as(u1, @trunc(inputs[0])) ^ @as(u1, @trunc(inputs[1]));
+                return 1.0 - @abs(correct - outputs[0]);
+            }
+        }.fitnessFn);
+        try population.select(ELITE_COUNT);
+
+        try stdout.print(
+            "Generation #{}'s highest fitness score: {}%\n",
+            .{ i + 1, population.networks[0].fitness / 4.0 * 100.0 },
+        );
+        try stdout.flush();
+    }
 }
